0874ea68f06648fa9be56428cc7c49df84373aa5
Velmurugan-12 <mvelmurugan2192@gmail.com>
2025-03-01 15:55:45 +0530

    loader file is created

--- src/utils/SpinnerLoader.jsx
+++ src/utils/SpinnerLoader.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect} from 'react';
 
 const SpinnerLoader = () => {
   const [currentProgress, setCurrentProgress] = useState(0)
