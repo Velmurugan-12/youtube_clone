6f4fa75b2e371531f62c26c20426a19c0cbac67d
Velmurugan-12 <mvelmurugan2192@gmail.com>
2025-03-01 15:53:17 +0530

    SpeechRecognition file is added

--- src/useContextHook/useSpeechRecognition.jsx
+++ src/useContextHook/useSpeechRecognition.jsx
@@ -3,7 +3,6 @@ import 'regenerator-runtime/runtime';
 
 import SpeechRecognition, {useSpeechRecognition} from "react-speech-recognition";
 
-
 const useSpeechRecognitions = (setSearchQuery) =>{
     const {
         transcript,
